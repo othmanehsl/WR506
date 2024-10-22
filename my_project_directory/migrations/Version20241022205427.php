82ac4576583326edba440543557b2c2512b3aadd
Github MMI3 <github@mmi-troyes.fr>
2024-10-22 22:23:39 +0000

    fix : Correction de l'authentification des users

--- my_project_directory/migrations/Version20241022205427.php
+++ my_project_directory/migrations/Version20241022205427.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240924095734 extends AbstractMigration
+final class Version20241022205427 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240924095734 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actor CHANGE nationalty nationality VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE media_object (id INT AUTO_INCREMENT NOT NULL, file_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actor CHANGE nationality nationalty VARCHAR(255) NOT NULL');
+        $this->addSql('DROP TABLE media_object');
     }
 }
